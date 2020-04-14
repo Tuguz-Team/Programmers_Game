9f0d6b58789fbca97403aa914730e198ed671a42
tuguzT <56771526+tuguzT@users.noreply.github.com>
2020-04-14 15:29:33 +0300

    Исправил ошибки в открытии игры на PC (класс DesktopLauncher)

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,12 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.programmers.enums.Difficulty;
-import com.programmers.screens.GameScreen;
+import com.programmers.screens.ScreenLoader;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GameScreen(Difficulty.Hard, 4), config);
+		new LwjglApplication(new ScreenLoader(), config);
 	}
 }
